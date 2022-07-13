85d82c82d2d5d1f1bcfaeba6c299fb2b2485fe45
Bruno Neves <bneves@Brunos-MacBook-Pro.local>
2022-07-12 23:02:19 -0300

    Branch to test maps

---
+++ SImple_Map/static/js/maplot.js
@@ -0,0 +1,14 @@
+console.log("FUNFA PO");
+
+// Map object with a center and zoom level
+let myMap = L.map('mapid').setView([40.7,-94.5], 4);
+
+// Tile layer to be the background of the map
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+// Testing GRAYMAP layer
+streets.addTo(myMap);
+
