71fddf6d00f3259e56f476a93b411ba61fc75f5e
Bruno Neves <bneves@Brunos-MacBook-Pro.local>
2022-07-18 09:52:44 -0300

    MultiMarkers

--- SImple_Map/static/js/maplot.js
+++ SImple_Map/static/js/maplot.js
@@ -1,7 +1,7 @@
-console.log("FUNFA PO");
+
 
 // Map object with a center and zoom level
-let myMap = L.map('mapid').setView([34.0522,-118.2437], 12);
+let myMap = L.map('mapid').setView([34.0522,-90.2437], 4);
 
 // Tile layer to be the background of the map
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -12,6 +12,16 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 // Testing GRAYMAP layer
 streets.addTo(myMap);
 
+// Get data from cities.js
+let cityData = cities;
+
 
 // Let's go to LA
-var marker = L.circleMarker([34.0522,-118.2437], {radius:300, color:"black", fillColor:"#ffffa1"}).addTo(myMap);
+// var citiess = L.circleMarker([34.0522,-118.2437], {radius:300, color:"black", fillColor:"#ffffa1"}).addTo(myMap);
+
+
+  // Looping through every city
+cityData.forEach(function(city)
+    {console.log(city)
+     L.circleMarker(city.location, {radius:(city.population)/100000, color:"orange", colorFill:"orange"}).bindPopup("<h2>"+city.city+", "+city.state+"</h> <hr> <h3>Population "+city.population.toLocaleString()+"</h3>").addTo(myMap);
+    });
